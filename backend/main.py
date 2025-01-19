d13dc64880f7bd4c761b518c413537ed6f7e269c
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2025-01-19 07:27:33 -0500

    Add FastAPI backend for LLM

---
+++ backend/main.py
@@ -0,0 +1,53 @@
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from groq import Groq
+
+import os
+
+from rmp import *
+
+_ = load_dotenv()
+
+app = FastAPI()
+groq = Groq(api_key=os.environ["GROQ_KEY"])
+
+
+@app.get("/", description="Basic endpoint to verify that the API is running.")
+def read_root():
+    return {"status": "Up and running."}
+
+
+def format_feedback(feedback: Any):
+    node = feedback["node"]
+    return f"<userFeedback>\n<clarityRating>{node['clarityRating']}/5.0</clarityRating>\n<difficultyRating>{node['difficultyRating']}/5.0</difficultyRating>\n<helpfulRating>{node['helpfulRating']}/5.0</helpfulRating>\n<comment>{node['comment']}</comment>\n</userFeedback>"
+
+
+@app.get(
+    "/prof_feedback/{id}/{course}/{course_display}/{prompt}",
+    description="Uses Groq to get information about a professor based on Rate My Professors comments.",
+    response_description="The LLM completion result.",
+)
+def do_stuff(id: str, course: str, course_display: str, prompt: str):
+    # Get comments about prof
+    comments = get_professor_info(id, course)
+    if comments is None:
+        return None
+    # Groq completion
+    prompt_comments = "\n".join(map(format_feedback, comments["ratings"]["edges"]))
+
+    chat_completion = groq.chat.completions.create(
+        messages=[
+            {
+                "role": "system",
+                "content": "You are an assistant designed to help users get information on their professors at the University of Ottawa. The following is information on a particular professor. Answer any questions that a user may have about this professor. All ratings are on a five-point scale unless otherwise specified.",
+            },
+            {
+                "role": "system",
+                "content": f"<professorName>{comments['firstName']} {comments['lastName']}</professorName>\n<avgRating>{comments['avgRating']}</avgRating>\n<avgDifficulty>{comments['avgDifficulty']}</avgDifficulty>\n<courseCode>{course}</courseCode>\n<courseCodeDisplay>{course_display}</courseCodeDisplay>\n<userFeedbackSection>\n{prompt_comments}\n</userFeedbackSection>",
+            },
+            {"role": "user", "content": prompt},
+        ],
+        model="llama-3.3-70b-versatile",
+        temperature=0.1,
+    )
+    return chat_completion.choices[0].message.content
---
+++ backend/rmp.py
@@ -0,0 +1,30 @@
+"""Wrappers around Rate My Professors APIs."""
+
+from typing import Any
+import requests
+
+import base64
+import json
+
+ENDPOINT = "https://www.ratemyprofessors.com/graphql"
+AUTH = f"Basic {base64.b64encode('test:test'.encode('utf-8')).decode()}"
+SCHOOL_ID = base64.b64encode("School-1452".encode("utf-8")).decode()
+
+
+def get_professor_info(id: str, course: str) -> Any | None:
+    r = requests.post(
+        ENDPOINT,
+        headers={
+            "Accept": "*/*",
+            "Authorization": AUTH,
+            "Access-Control-Allow-Origin": "*",
+            "Content-Type": "application/json",
+        },
+        json={
+            "query": f"""query {{ node(id: {json.dumps(id)} ) {{ ... on Teacher {{ firstName lastName avgRating avgDifficulty ratings(first: 25, courseFilter: {json.dumps(course)}) {{ edges {{ node {{ comment helpfulRating difficultyRating clarityRating }} }} }} }} }}  }}"""
+        },
+    )
+    response = r.json()
+    if "errors" in response:
+        return None
+    return response["data"]["node"]
